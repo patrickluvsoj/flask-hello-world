573c271b50376dce4cca3d0cbe3d17cc6abb54cb
patrickluvsoj <pasuzuk@microsoft.com>
2018-12-09 19:41:47 -0800

    added dynamic routes

--- app.py
+++ app.py
@@ -3,11 +3,35 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/hello")
-
 def hello_world():
     return "Hi, there!"
 
+@app.route("/search/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def name(name):
+    if name.lower() == "patrick":
+        return "Hello, " + name, 200
+    else:
+        return "Not Found", 404
+
+
 if __name__ == "__main__":
     app.run()
-    
